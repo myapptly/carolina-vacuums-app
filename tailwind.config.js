0eed41d03a5356226a70a719fd664e366cb2d39a
myapptly <tbledsoe1954@gmail.com>
2026-07-21 12:46:36 -0400

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        navy: '#1a3a6e',
+        vacgreen: '#7cb87c',
+      },
+    },
+  },
+  plugins: [],
+} 
